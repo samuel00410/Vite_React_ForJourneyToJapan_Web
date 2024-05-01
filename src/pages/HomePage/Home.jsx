ce53bf032e5e83feb0ac5bf7a8a2d2231c88507b
samuel00410 <samuel00410@gmail.com>
2024-05-01 23:13:57 +0800

    輪播背景圖片上傳

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./Home.scss";
+import backgroundImg1 from "/images/日本櫻花.jpg";
+import backgroundImg2 from "/images/大阪街頭.jpg";
+import backgroundImg3 from "/images/富士山2.jpg";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [imageIndex, setImageIndex] = useState(0);
+  // 創建一個陣列，包含三張背景圖片的路徑，每三秒切換一次背景圖片
+  const backgroundImgs = [backgroundImg1, backgroundImg2, backgroundImg3];
+  useEffect(() => {
+    setInterval(() => {
+      setImageIndex((prevIndex) => (prevIndex + 1) % 3);
+    }, 3000);
+
+    return () => {
+      clearInterval();
+    };
+  });
+
+  return (
+    <>
+      <section
+        className="slide-img"
+        style={{ backgroundImage: `url(${backgroundImgs[imageIndex]})` }}
+      >
+        <div className="filter"></div>
+        <div className="travel-description">
+          <p>
+            休閒空間，特殊假期。
+            <br />
+            安排一趟日本旅遊行程，放鬆身心。
+          </p>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
