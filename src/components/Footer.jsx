30a90f42d72727b7ccec8afd62e6ed3a2850b744
samuel00410 <samuel00410@gmail.com>
2024-05-03 18:29:07 +0800

    註解上傳

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,12 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <footer>
+      {/* Logo區塊 */}
       <section className="footer-logo">
         <img src="/images/網站Logo.svg" alt="" />
         <h3>2024日本旅遊推薦</h3>
       </section>
+      {/* 連結區塊 */}
       <section className="links-icons">
         <a href="#">
           <img src="/Icons/facebook.svg" alt="fb" />
