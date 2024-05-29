10e3b2c9fc6f7f54d5ce9d8281ec402dcd092d66
samuel00410 <samuel00410@gmail.com>
2024-05-29 20:41:09 +0800

    上傳

--- src/pages/TripBookingPage/TripBook.jsx
+++ src/pages/TripBookingPage/TripBook.jsx
@@ -5,20 +5,7 @@ import OpenAI from "openai";
 import img1 from "/images/富士山.png";
 
 const TripBook = () => {
-  const [count, setCount] = useState(0);
-  const [data, setData] = useState({ name: "Alice" });
-
-  useEffect(() => {
-    console.log("執行useEffect...");
-  }, [data]); // data 是对象，引用在每次渲染时都会变化
-
-  return (
-    <div>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>增加Count按鈕</button>
-      <button onClick={() => setData({ name: "Alice" })}>更新data按鈕</button>
-    </div>
-  );
+  return <></>;
 };
 
 export default TripBook;
